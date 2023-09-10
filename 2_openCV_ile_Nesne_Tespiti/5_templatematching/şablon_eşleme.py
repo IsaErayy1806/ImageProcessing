7682425820710ad5f73df2ea87453e8de8210dfd
IsaErayy1806 <aslanolisa@gmail.com>
2023-09-10 17:30:15 +0300

    deneme1

--- 2_openCV_ile_Nesne_Tespiti/5_templatematching/şablon_eşleme.py
+++ 2_openCV_ile_Nesne_Tespiti/5_templatematching/şablon_eşleme.py
@@ -1,18 +1,15 @@
-"""
-Şablon eşleştirme, bir şablon görüntünün konumunu daha büyük bir görüntüde aramak ve bulmak için bie yöntemdir.
-Şablon görüntüsünü giriş görüntüsün üzerine kaydırır ve şablon görüntüsünün altındaki giriş görüntüsünün şablonu ve yamayı karşılaştırır
-Çablon kaynak resmin neresindeyse o kısım kararıyor veya aydınlanıyor
-"""
-
 import cv2
 import matplotlib.pyplot as plt
 import os
+
+
 #template matching : şablon eşleme
 
-img=cv2.imread("C:\\Users\\aslan\\OneDrive\\Masaüstü\\python\\2_openCV_ile_Nesne_Tespiti\\5_templatematching\\cat.jpg",0)
-print(img.shape)
-
-template=cv2.imread("C:\\Users\\aslan\\OneDrive\\Masaüstü\\python\\2_openCV_ile_Nesne_Tespiti\\5_templatematching\\cat_face.jpg",0)
+img=cv2.imread("cat1.jpg",0)
+print(type(img))
+"""
+template=cv2.imread("cat_face1.jpg",0)
 print(template.shape)
 
 h,w=template.shape
+"""
